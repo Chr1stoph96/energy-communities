b64997747e66efe3f71b1905a2210461b4499757
Chr1stoph96 <christoph.meyer96@gmail.com>
2025-04-21 17:58:40 +0200

    Initial commit

---
+++ src/main/java/com/example/energy_communities/EnergyCommunitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.energy_communities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyCommunitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyCommunitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/energy_communities/EnergyCommunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.energy_communities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyCommunitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
