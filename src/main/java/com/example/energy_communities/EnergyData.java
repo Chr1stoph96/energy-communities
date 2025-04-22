5cdde1c6bd4f118847d48cfca3708812f5ae612f
Chr1stoph96 <christoph.meyer96@gmail.com>
2025-04-22 16:28:52 +0200

    Storage Class "EnergyData" added

---
+++ src/main/java/com/example/energy_communities/EnergyData.java
@@ -0,0 +1,72 @@
+package com.example.energy_communities;
+
+import java.time.LocalDateTime;
+
+public class EnergyData {
+    private LocalDateTime hour;
+    private double communityProduced;
+    private double communityUsed;
+    private double gridUsed;
+    private double communityDepleted;
+    private double gridPortion;
+
+    public EnergyData() {
+    }
+
+    public EnergyData(LocalDateTime hour, double communityProduced, double communityUsed, double gridUsed, double communityDepleted, double gridPortion) {
+        this.hour = hour;
+        this.communityProduced = communityProduced;
+        this.communityUsed = communityUsed;
+        this.gridUsed = gridUsed;
+        this.communityDepleted = communityDepleted;
+        this.gridPortion = gridPortion;
+    }
+
+    public LocalDateTime getHour() {
+        return hour;
+    }
+
+    public void setHour(LocalDateTime hour) {
+        this.hour = hour;
+    }
+
+    public double getCommunityProduced() {
+        return communityProduced;
+    }
+
+    public void setCommunityProduced(double communityProduced) {
+        this.communityProduced = communityProduced;
+    }
+
+    public double getCommunityUsed() {
+        return communityUsed;
+    }
+
+    public void setCommunityUsed(double communityUsed) {
+        this.communityUsed = communityUsed;
+    }
+
+    public double getGridUsed() {
+        return gridUsed;
+    }
+
+    public void setGridUsed(double gridUsed) {
+        this.gridUsed = gridUsed;
+    }
+
+    public double getCommunityDepleted() {
+        return communityDepleted;
+    }
+
+    public void setCommunityDepleted(double communityDepleted) {
+        this.communityDepleted = communityDepleted;
+    }
+
+    public double getGridPortion() {
+        return gridPortion;
+    }
+
+    public void setGridPortion(double gridPortion) {
+        this.gridPortion = gridPortion;
+    }
+}
